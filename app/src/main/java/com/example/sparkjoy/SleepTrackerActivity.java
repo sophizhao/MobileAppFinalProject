15039401b1c4d0a37867b590586bab1ddbf5fa9d
costigane <emily.rae.costigan@gmail.com>
2022-10-31 08:13:20 -0500

    firebase connection added, sleep and water tracker activities made

---
+++ app/src/main/java/com/example/sparkjoy/SleepTrackerActivity.java
@@ -0,0 +1,14 @@
+package com.example.sparkjoy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SleepTrackerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sleep_tracker);
+    }
+}
---
+++ app/src/main/java/com/example/sparkjoy/WaterLogActivity.java
@@ -0,0 +1,14 @@
+package com.example.sparkjoy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WaterLogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water_log);
+    }
+}
