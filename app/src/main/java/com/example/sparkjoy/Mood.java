dcf4f835a2ab76d257d8d599543cb10fe6eded48
sophizhao <90772792+sophizhao@users.noreply.github.com>
2022-10-31 08:19:06 -0500

    date implemented
    please find out what to do about imagebtn parameters?!

--- app/src/main/java/com/example/sparkjoy/Mood.java
+++ app/src/main/java/com/example/sparkjoy/Mood.java
@@ -1,10 +1,23 @@
 package com.example.sparkjoy;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 import java.util.Date;
+import java.time.LocalDate;
+
+
 
 public class Mood {
-    private Date dateCreated;
+    public LocalDate dateCreated;
     private int mood;
     /*
     1 - red
@@ -12,11 +25,28 @@ public class Mood {
     3 - green
     4 - blue
      */
-    SimpleDateFormat dateFormat = new SimpleDateFormat("MM/dd/yyyy");
 
-    public Mood(Date dateCreated, int mood) {
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public Mood(int mood) throws ParseException {
+        this.dateCreated = java.time.LocalDate.now();
+        System.out.println(dateCreated);
         this.mood = mood;
     }
 
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public int getMood() {
+        return mood;
+    }
+
+    public void setMood(int mood) {
+        this.mood = mood;
+    }
 }
--- app/src/main/java/com/example/sparkjoy/MoodMeterActivity.java
+++ app/src/main/java/com/example/sparkjoy/MoodMeterActivity.java
@@ -1,13 +1,22 @@
 package com.example.sparkjoy;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import java.text.ParseException;
+import java.util.ArrayList;
+
 public class MoodMeterActivity extends AppCompatActivity {
     ImageButton red, yellow, green, blue;
+    private ArrayList<Mood> myMoods = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,36 +29,47 @@ public class MoodMeterActivity extends AppCompatActivity {
         blue = findViewById(R.id.blueButton);
     }
 
-    public void moodSelected(int mood){
-        /**
-         * take in param
-         * check to see if mood has been selected already (you can only select one mood a day)
-         * - do this by looking in firebase (see below)
-         * if done: update mood to new selection
-         * if not done: add new mood attached to today's date
-         *
-         * myMemories.clear();        // empties the AL so that it can get a fresh copy of data
-         *         db.collection("users").document(uid).collection("myMemoryList")
-         *                 .get()
-         *                 .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-         *                     @Override
-         *                     public void onComplete(@NonNull Task<QuerySnapshot> task) {
-         *                         if (task.isSuccessful()) {
-         *                             for (DocumentSnapshot doc: task.getResult()) {
-         *                                 Memory memory = doc.toObject(Memory.class);
-         *                                 myMemories.add(memory);
-         *                             }
-         *
-         *                             Log.i(TAG, "Success reading data: "+ myMemories.toString());
-         *                             firestoreCallback.onCallback(myMemories);
-         *                         }
-         *                         else {
-         *                             Log.d(TAG, "Error getting documents: " + task.getException());
-         *                         }
-         *                     }
-         *                 });
-         */
+
+    /**
+     * take in param
+     * check to see if mood has been selected already (you can only select one mood a day)
+     * - do this by looking in firebase (see below)
+     * if done: update mood to new selection
+     * if not done: add new mood attached to today's date
+     *
+
+     */
+    @RequiresApi(api = Build.VERSION_CODES.O)
+    public void moodSelected(int mood) throws ParseException {
+        boolean alreadyAdded = false;
+        for(int i = 0; i < myMoods.size(); i++){
+            if (myMoods.get(i).getDateCreated().isEqual(java.time.LocalDate.now())){ //if it is today
+                myMoods.get(i).setMood(mood);
+                alreadyAdded = true;
+            }
+        }
+        if (!alreadyAdded){
+            myMoods.add(new Mood(mood));
+        }
+//        myMoods.clear();        // empties the AL so that it can get a fresh copy of data
+//        db.collection("users").document(uid).collection("myMemoryList")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (DocumentSnapshot doc: task.getResult()) {
+//                                Memory memory = doc.toObject(Memory.class);
+//                                myMemories.add(memory);
+//                            }
+//
+//                            Log.i(TAG, "Success reading data: "+ myMemories.toString());
+//                            firestoreCallback.onCallback(myMemories);
+//                        }
+//                        else {
+//                            Log.d(TAG, "Error getting documents: " + task.getException());
+//                        }
+//                    }
+//                });
     }
-
-
 }
