664e7b109c99622cc4ad00b54e3871d02977f018
sophizhao <90772792+sophizhao@users.noreply.github.com>
2022-11-07 08:18:37 -0600

    calendar still doesnt work but it works in my practice project.......

--- app/src/main/java/com/example/sparkjoy/CalendarActivity.java
+++ app/src/main/java/com/example/sparkjoy/CalendarActivity.java
@@ -1,13 +1,17 @@
 package com.example.sparkjoy;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.CalendarView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.time.Instant;
 import java.time.LocalDate;
@@ -15,19 +19,33 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 
 public class CalendarActivity extends AppCompatActivity {
+    TextView moodDisplay;
+    CalendarView calendar;
+    long mms;
+    final String TAG = "Sparky";
 
-    CalendarView calendar = findViewById(R.id.calendarView);
-    TextView moodDisplay = findViewById(R.id.moodTV);
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        moodDisplay = findViewById(R.id.moodTV);
+        calendar = (CalendarView) findViewById(R.id.calendarView);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calendar);
+//        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+//            @Override
+//            public void onSelectedDayChange(CalendarView calendarView, int i, int i1, int i2) {
+//                mms = calendar.getDate();
+//                Log.d(TAG, "Date Changed (mms): " + mms);
+//                loadDate();
+//            }
+//        });
+
         calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @RequiresApi(api = Build.VERSION_CODES.O)
             @Override
-            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
-                loadDate();
+            public void onSelectedDayChange(CalendarView calendarView, int i, int i1, int i2) {
+                mms = calendar.getDate();
+                Log.d(TAG, "onSelectedDayChange: mm/dd/yyyy: " + mms);
             }
         });
 
@@ -50,7 +68,7 @@ public class CalendarActivity extends AppCompatActivity {
     @RequiresApi(api = Build.VERSION_CODES.O)
     public Mood findMoodSelected(){
         //find selected date in milliseconds and convert to LocalDate
-        LocalDate date = Instant.ofEpochMilli(calendar.getDate()).atZone(ZoneId.systemDefault()).toLocalDate();
+        LocalDate date = Instant.ofEpochMilli(mms).atZone(ZoneId.systemDefault()).toLocalDate();
         for (Mood currentMood : MoodMeterActivity.myMoods) {
             if (currentMood.getDateCreated().equals(date)) {
                 // Found matching mood
