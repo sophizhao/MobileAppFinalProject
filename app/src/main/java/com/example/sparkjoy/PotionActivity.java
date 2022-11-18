47d6941d14a1185114739fc183b91d0bb76de7f7
costigane <emily.rae.costigan@gmail.com>
2022-11-18 08:11:50 -0600

    graph designing

--- app/src/main/java/com/example/sparkjoy/PotionActivity.java
+++ app/src/main/java/com/example/sparkjoy/PotionActivity.java
@@ -2,6 +2,7 @@ package com.example.sparkjoy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.jjoe64.graphview.GraphView;
@@ -17,14 +18,26 @@ public class PotionActivity extends AppCompatActivity {
         GraphView graph = (GraphView) findViewById(R.id.graph);
 
         LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
                 new DataPoint(1, 5),
                 new DataPoint(2, 3),
                 new DataPoint(3, 2),
-                new DataPoint(4, 6)
+                new DataPoint(4, 6),
+                new DataPoint(5, 6),
+                new DataPoint(6, 5),
+                new DataPoint(7, 3)
         });
 
+        series.setColor(Color.rgb(215, 130, 255));
+        series.setThickness(12);
+        series.setDrawBackground(true);
+        series.setBackgroundColor(Color.argb(80, 215, 130, 255));
+        series.setDrawDataPoints(true);
+        series.setDataPointsRadius(15);
+
         graph.addSeries(series);
+        graph.getViewport().setXAxisBoundsManual(true);
+        graph.getViewport().setMinX(1);
+        graph.getViewport().setMaxX(7);
     }
 
 }
